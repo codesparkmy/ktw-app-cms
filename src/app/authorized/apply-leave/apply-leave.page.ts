3060fcd903247ddab3071498aedd60294ab75dd4
Hakim Mazlan <hakimmazlan@Hakims-MacBook-Air.local>
2022-09-09 00:59:21 +0800

    apply leave fix UI

--- src/app/authorized/apply-leave/apply-leave.page.ts
+++ src/app/authorized/apply-leave/apply-leave.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { format, parseISO } from 'date-fns';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
   selector: 'app-apply-leave',
@@ -8,6 +9,7 @@ import { format, parseISO } from 'date-fns';
   styleUrls: ['./apply-leave.page.scss'],
 })
 export class ApplyLeavePage implements OnInit {
+  picture:string;
   dateValue = format(new Date(), 'd MMM yyyy');
   endDateString = '';
   startDateString = '';
@@ -18,24 +20,35 @@ export class ApplyLeavePage implements OnInit {
 
   ngOnInit() {}
 
-  endDate(){
-    this.endDateString = format(new Date(),'d MMM yyyy')
+  endDate() {
+    this.endDateString = format(new Date(), 'd MMM yyyy');
   }
 
-  startDate(){
-    this.startDateString = format(new Date(),'d MMM yyyy')
+  startDate() {
+    this.startDateString = format(new Date(), 'd MMM yyyy');
   }
-
 
   backClicked() {
     this.navCtrl.back();
   }
 
-  endDateChanged(value){
-    this.endDateString = format(parseISO(value), 'd MMM yyyy')
+  endDateChanged(value) {
+    console.log(value);
+    this.endDateString = format(parseISO(value), 'd MMM yyyy');
   }
 
-  startDateChanged(value){
-    this.startDateString = format(parseISO(value), 'd MMM yyyy')
+  startDateChanged(value) {
+    this.startDateString = format(parseISO(value), 'd MMM yyyy');
+  }
+
+  async openGallery() {
+    console.log('open gallery');
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.DataUrl,
+    });
+
+    this.picture = image.dataUrl;
   }
 }

bb4e85e4abf145703b19c92e4cb8168d3b739e10
Hakim Mazlan <hakimmazlan@Hakims-MacBook-Air.local>
2022-09-12 02:36:39 +0800

    leave ui updates

--- src/app/authorized/leave/leave.page.ts
+++ src/app/authorized/leave/leave.page.ts
@@ -29,12 +29,78 @@ export class LeavePage implements OnInit, AfterViewInit, OnDestroy {
   breakpoint = null;
   initBreakpoint = null;
   menuSubscriber?: Subscription;
-
+  requestLeave = [
+    {
+      leaveType: 'Annual',
+      reason: 'I would like to take out some days.',
+      status: 'rejected',
+    },
+    {
+      leaveType: 'Medical',
+      reason: 'I would like to take out some days.',
+      status: 'approved',
+    },
+    {
+      leaveType: 'Emergency',
+      reason: 'I would like to take out some days.',
+      status: 'Rejected',
+    },
+    {
+      leaveType: 'Emergency',
+      reason: 'I would like to take out some days.',
+      status: 'pending',
+    },
+    {
+      leaveType: 'Annual',
+      reason: 'I would like to take out some days.',
+      status: 'approved',
+    },
+    {
+      leaveType: 'Medical',
+      reason: 'I would like to take out some days.',
+      status: 'approved',
+    },
+    {
+      leaveType: 'Emergency',
+      reason: 'I would like to take out some days.',
+      status: 'Rejected',
+    },
+    {
+      leaveType: 'Emergency',
+      reason: 'I would like to take out some days.',
+      status: 'pending',
+    },
+    {
+      leaveType: 'Annual',
+      reason: 'I would like to take out some days.',
+      status: 'approved',
+    },
+  ];
   leaveTypes = [
+    {
+      days: 7,
+      maxDays: 14,
+      type: 'Annual',
+    },
+    {
+      days: 10,
+      maxDays: 20,
+      type: 'Medical',
+    },
+    {
+      days: 8,
+      maxDays: 10,
+      type: 'Emergency',
+    },
+    {
+      days: 90,
+      maxDays: 90,
+      type: 'Paternity',
+    },
     {
       days: 14,
       maxDays: 14,
-      type: 'Annual',
+      type: 'Public',
     },
   ];
   constructor(
